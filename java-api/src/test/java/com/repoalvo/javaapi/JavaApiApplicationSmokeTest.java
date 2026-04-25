e3d2671875811667c9910e830fbc946d889493a2
Eneri Junior <jr.icm.sc@gmail.com>
2026-04-25 00:30:21 -0300

    fix: estabilizar testes java e garantir paridade de erro com python

--- java-api/src/test/java/com/repoalvo/javaapi/JavaApiApplicationSmokeTest.java
+++ java-api/src/test/java/com/repoalvo/javaapi/JavaApiApplicationSmokeTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class JavaApiApplicationSmokeTest {
